de95f3f399a40b422ae2c111ae4ac0e628dc9493
Gary Moore <madmangaz@gmail.com>
2022-02-17 19:51:46 +0000

    add some more scaffolding including CI

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+	std::string simple_greeting = "Hi Mom!";
+	std::cout << simple_greeting << std::endl;
+	return EXIT_SUCCESS;
+}
+
---
+++ tests/empty_tests.test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(EmptyTests, AssertTrue) {
+	ASSERT_TRUE(true);
+}
+
